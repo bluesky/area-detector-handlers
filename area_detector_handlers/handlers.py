8c52c81247882ea59d697536830c223bbfc03c8a
Thomas A Caswell <tcaswell@bnl.gov>
2019-09-25 15:28:51 -0400

    STY: apply black

--- area_detector_handlers/handlers.py
+++ area_detector_handlers/handlers.py
@@ -18,11 +18,10 @@ class IntegrityError(Exception):
 
 
 class AreaDetectorSPEHandler(HandlerBase):
-    specs = {'AD_SPE'} | HandlerBase.specs
+    specs = {"AD_SPE"} | HandlerBase.specs
 
-    def __init__(self, fpath, template, filename,
-                 frame_per_point=1):
-        self._path = os.path.join(fpath, '')
+    def __init__(self, fpath, template, filename, frame_per_point=1):
+        self._path = os.path.join(fpath, "")
         self._fpp = frame_per_point
         self._template = template
         self._filename = filename
@@ -30,9 +29,7 @@ class AreaDetectorSPEHandler(HandlerBase):
 
     def __call__(self, point_number):
         if point_number not in self._f_cache:
-            fname = self._template % (self._path,
-                                      self._filename,
-                                      point_number)
+            fname = self._template % (self._path, self._filename, point_number)
             spe_obj = PrincetonSPEFile(fname)
             self._f_cache[point_number] = spe_obj
 
@@ -40,22 +37,23 @@ class AreaDetectorSPEHandler(HandlerBase):
         data = spe.getData()
 
         if data.shape[0] != self._fpp:
-            raise IntegrityError("expected {} frames, found {} frames".format(
-                                 self._fpp, data.shape[0]))
+            raise IntegrityError(
+                "expected {} frames, found {} frames".format(self._fpp, data.shape[0])
+            )
         return data.squeeze()
 
     def get_file_list(self, datum_kwarg_gen):
-        return [self._template % (self._path,
-                                  self._filename,
-                                  d['point_number'])
-                for d in datum_kwarg_gen]
+        return [
+            self._template % (self._path, self._filename, d["point_number"])
+            for d in datum_kwarg_gen
+        ]
 
 
 class AreaDetectorTiffHandler(HandlerBase):
-    specs = {'AD_TIFF'} | HandlerBase.specs
+    specs = {"AD_TIFF"} | HandlerBase.specs
 
     def __init__(self, fpath, template, filename, frame_per_point=1):
-        self._path = os.path.join(fpath, '')
+        self._path = os.path.join(fpath, "")
         self._fpp = frame_per_point
         self._template = template
         self._filename = filename
@@ -96,6 +94,7 @@ class HDF5DatasetSliceHandler(HandlerBase):
         Open the hdf5 file in SWMR read mode. Only used when mode = 'r'.
         Default is False.
     """
+
     specs = set()
 
     def __init__(self, filename, key, frame_per_point=1):
@@ -122,7 +121,7 @@ class HDF5DatasetSliceHandler(HandlerBase):
         if self._file:
             return
 
-        self._file = h5py.File(self._filename, 'r')
+        self._file = h5py.File(self._filename, "r")
 
     def close(self):
         super().close()
@@ -144,13 +143,14 @@ class AreaDetectorHDF5Handler(HDF5DatasetSliceHandler):
     frame_per_point : integer, optional
         number of frames to return as one datum, default 1
     """
-    specs = {'AD_HDF5'} | HDF5DatasetSliceHandler.specs
+
+    specs = {"AD_HDF5"} | HDF5DatasetSliceHandler.specs
 
     def __init__(self, filename, frame_per_point=1):
-        hardcoded_key = '/entry/data/data'
+        hardcoded_key = "/entry/data/data"
         super().__init__(
-            filename=filename, key=hardcoded_key,
-            frame_per_point=frame_per_point)
+            filename=filename, key=hardcoded_key, frame_per_point=frame_per_point
+        )
 
 
 class AreaDetectorHDF5SWMRHandler(AreaDetectorHDF5Handler):
@@ -167,19 +167,19 @@ class AreaDetectorHDF5SWMRHandler(AreaDetectorHDF5Handler):
     frame_per_point : integer, optional
         number of frames to return as one datum, default 1
     """
-    specs = {'AD_HDF5_SWMR'} | HDF5DatasetSliceHandler.specs
+
+    specs = {"AD_HDF5_SWMR"} | HDF5DatasetSliceHandler.specs
 
     def open(self):
         if self._file:
             return
 
-        self._file = h5py.File(self._filename, 'r', swmr=True)
+        self._file = h5py.File(self._filename, "r", swmr=True)
 
     def __call__(self, point_number):
         if self._dataset is not None:
             self._dataset.id.refresh()
-        rtn = super().__call__(
-            point_number)
+        rtn = super().__call__(point_number)
 
         return rtn
 
@@ -196,13 +196,16 @@ class AreaDetectorHDF5TimestampHandler(HandlerBase):
     frame_per_point : integer, optional
         number of frames to return as one datum, default 1
     """
-    specs = {'AD_HDF5_TS'} | HandlerBase.specs
+
+    specs = {"AD_HDF5_TS"} | HandlerBase.specs
 
     def __init__(self, filename, frame_per_point=1):
         self._fpp = frame_per_point
         self._filename = filename
-        self._key = ['/entry/instrument/NDAttributes/NDArrayEpicsTSSec',
-                     '/entry/instrument/NDAttributes/NDArrayEpicsTSnSec']
+        self._key = [
+            "/entry/instrument/NDAttributes/NDArrayEpicsTSSec",
+            "/entry/instrument/NDAttributes/NDArrayEpicsTSnSec",
+        ]
         self._file = None
         self._dataset1 = None
         self._dataset2 = None
@@ -222,7 +225,7 @@ class AreaDetectorHDF5TimestampHandler(HandlerBase):
     def open(self):
         if self._file:
             return
-        self._file = h5py.File(self._filename, 'r')
+        self._file = h5py.File(self._filename, "r")
 
     def close(self):
         super().close()
@@ -243,27 +246,26 @@ class AreaDetectorHDF5SWMRTimestampHandler(AreaDetectorHDF5TimestampHandler):
     frame_per_point : integer, optional
         number of frames to return as one datum, default 1
     """
-    specs = {'AD_HDF5_SWMR_TS'} | HandlerBase.specs
+
+    specs = {"AD_HDF5_SWMR_TS"} | HandlerBase.specs
 
     def open(self):
         if self._file:
             return
-        self._file = h5py.File(self._filename, 'r', swmr=True)
+        self._file = h5py.File(self._filename, "r", swmr=True)
 
     def __call__(self, point_number):
         if (self._dataset1 is not None) and (self._dataset2 is not None):
             self._dataset.id.refresh()
-        rtn = super().__call__(
-            point_number)
+        rtn = super().__call__(point_number)
         return rtn
 
 
 class PilatusCBFHandler:
-    specs = {'AD_CBF'}
+    specs = {"AD_CBF"}
 
-    def __init__(self, rpath, template, filename, frame_per_point=1,
-                 initial_number=1):
-        self._path = os.path.join(rpath, '')
+    def __init__(self, rpath, template, filename, frame_per_point=1, initial_number=1):
+        self._path = os.path.join(rpath, "")
         self._fpp = frame_per_point
         self._template = template
         self._filename = filename
@@ -276,10 +278,12 @@ class PilatusCBFHandler:
             import fabio
         except ImportError as exc:
             raise ImportError(
-                "The AreaDetectorSPEHandler handler requires fabio to be "
-                "installed.") from exc
-        start, stop = (self._initial_number + point_number *
-                       self._fpp, (point_number + 2) * self._fpp)
+                "The AreaDetectorSPEHandler handler requires fabio to be " "installed."
+            ) from exc
+        start, stop = (
+            self._initial_number + point_number * self._fpp,
+            (point_number + 2) * self._fpp,
+        )
         ret = []
         # commented out by LY to test scan speed imperovement, 2017-01-24
         for j in range(start, stop):
@@ -291,9 +295,11 @@ class PilatusCBFHandler:
     def get_file_list(self, datum_kwargs_gen):
         file_list = []
         for dk in datum_kwargs_gen:
-            point_number = dk['point_number']
-            start, stop = (self._initial_number + point_number *
-                           self._fpp, (point_number + 2) * self._fpp)
+            point_number = dk["point_number"]
+            start, stop = (
+                self._initial_number + point_number * self._fpp,
+                (point_number + 2) * self._fpp,
+            )
             for j in range(start, stop):
                 fn = self._template % (self._path, self._filename, j)
                 file_list.append(fn)
