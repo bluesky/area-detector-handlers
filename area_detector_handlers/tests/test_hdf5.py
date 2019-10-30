8c24505b0e0306b2d6840624b663f2534a3bdbd8
Dan Allan <dallan@bnl.gov>
2019-10-30 09:03:57 -0400

    Add test for AD_HDF5.

---
+++ area_detector_handlers/tests/test_hdf5.py
@@ -0,0 +1,13 @@
+from area_detector_handlers.tests.conftest import select_handler
+import numpy as np
+
+
+@select_handler("AD_HDF5")
+def test_hdf5(hdf5_files, handler):
+    (rpath, kwargs), (N_rows, N_cols, N_points, fpp) = hdf5_files
+    expected_shape = (fpp, N_rows, N_cols)
+    with handler(rpath, **kwargs) as h:
+        for frame in range(N_points):
+            d = h(point_number=frame)
+            assert d.shape == expected_shape
+            assert np.all(d == frame)
